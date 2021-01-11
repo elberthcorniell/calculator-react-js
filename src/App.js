2336637d0ea1abea0a1a9514db83d38d18762d8b
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-11 14:43:03 -0400

    Linters fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     const { title } = this.props;
-    return <div className="App">
-      <h1>{title}</h1>
-    </div>
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+      </div>
+    );
   }
 }
 
 App.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
 };
+
+App.defaultProps = { title: 'Hi everyone!' };
 
 export default App;

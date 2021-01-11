041d0966ed4d8bcc46db7be5a0ee9c5fd8e6ce4e
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-11 11:42:47 -0400

    packages added and unused assets deleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    const { title } = this.porps;
+    return <div className="App">
+      <h1>{title}</h1>
     </div>
-  );
+  }
 }
+
+App.propTypes = {
+  title: PropTypes.string
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Hi, every one!" />
   </React.StrictMode>,
   document.getElementById('root')
 );

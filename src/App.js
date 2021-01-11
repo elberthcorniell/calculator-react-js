cac1ae4be345ce38f2802f285324fe5044dd3fb2
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-11 17:00:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { Component } from 'react';
 import { ButtonPanel, Display } from './components';
 import './App.css';
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Display />
-        <ButtonPanel />
-      </div>
-    );
-  }
-}
+
+const App = () => (
+  <>
+    <Display />
+    <ButtonPanel />
+  </>
+);
 
 export default App;

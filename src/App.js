5e604bfed638d68f39f99b8686369f393b0b4a22
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-11 16:28:01 -0400

    Updated old components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+import { ButtonPanel, Display } from './components';
 import './App.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
-    const { title } = this.props;
     return (
       <div className="App">
-        <h1>{title}</h1>
+        <Display />
+        <ButtonPanel />
       </div>
     );
   }
 }
-
-App.propTypes = {
-  title: PropTypes.string,
-};
-
-App.defaultProps = { title: 'Hi everyone!' };
 
 export default App;

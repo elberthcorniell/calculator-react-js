7a2de6dbd662ab1d0bd3efa13365e0baeb50414e
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-22 11:48:02 -0400

    Create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <h1>Math magicians</h1>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/calculator">Calculator</Link></li>
+      <li><Link to="/quote">Quote</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;

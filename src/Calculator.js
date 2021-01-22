236d3ffb1eca25761ac222427b3c9edb4eaf2880
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-22 11:02:43 -0400

    Create Calculator.js

---
+++ src/Calculator.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import { ButtonPanel, Display } from './components';
+import { calculate } from './logic';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <div className="calculator">
+        <Display result={total} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+export default App;
